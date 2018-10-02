39995a1516f4e81a2c740246fffac7e3712ed81d
Ahmad Dinkins <ahmadid2@illinois.edu>
2018-10-01 19:24:51 -0500

    deleted regex

--- assignment1/regex.py
+++
@@ -1,6 +0,0 @@
-import re
-
-#print(re.split(r'(s)', 'here are some words'))
-#print (re.split(r'[a-fA-F0-9p-q][a-f]', 'ngafjdkFnerjafk;dgnascdjkgnas',
-                #re.I|re.M))
-print (re.split(r'\d', 'ajfdk324 main st.fnuasjifnajis'))
